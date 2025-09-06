1a0e825c377bc062cef6ac55598beb391f2faf2e
Santhoshkumar S <santhoshkumartech17@gmail.com>
2025-09-06 20:24:40 +0530

    Update Demo.java

--- corejava/src/com/tcf/assignment_1encapusation/Demo.java
+++ corejava/src/com/tcf/assignment_1encapusation/Demo.java
@@ -2,4 +2,29 @@ package com.tcf.assignment_1encapusation;
 
 public class Demo {
 
+  public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		 BankAccount account = new BankAccount("12345", "Santhosh", 1000.0);
+
+	        // Display initial details
+	        account.displayAccountInfo();
+
+	        // Deposit money
+	        account.deposit(500.0);
+
+	        // Withdraw money
+	        account.withdraw(200.0);
+
+	        // Try withdrawing more than balance
+	        account.withdraw(2000.0);
+
+	        // Display final details
+	        account.displayAccountInfo();
+
+	}
+
+}
+  
+
 }
