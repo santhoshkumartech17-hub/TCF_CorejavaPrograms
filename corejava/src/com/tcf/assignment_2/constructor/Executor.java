f904a578ba61ddd9c6e06423533c4647b306898c
santhoshkumar <santhoshkumar@smarty>
2025-09-07 16:16:31 +0530

    commit

--- corejava/src/com/tcf/assignment_2/constructor/Executor.java
+++ corejava/src/com/tcf/assignment_2/constructor/Executor.java
@@ -1,13 +1,12 @@
 package com.tcf.assignment_2.constructor;
 
-
 public class Executor {
 	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		Student s1=new Student(1,"Santhosh","JFS");
-		Student s2=new Student(2,"Dharshana","JFS");
-		Student s3=new Student(3,"Aastha","DESIGNER");
+		Student s1=new Student(1,"Sugapathi","JFS");
+		Student s2=new Student(2,"Kiruthiga","JFS");
+		Student s3=new Student(3,"Nadenapathi","DESIGNER");
 		
 		
 		s1.displayInfo();
@@ -19,5 +18,6 @@ public class Executor {
 	}
 
 }
+	
 
-
+}
